c3d55940a1b458f4c9cf515cbb26a692c4407d46
V <monisha.v@capgemini.com>
2023-12-26 15:47:12 +0530

    Reviewed code after alteration

--- MortgageCalculator.java
+++ MortgageCalculator.java
@@ -1,30 +1,39 @@
 package Java;
 import java.text.NumberFormat;
+import java.time.LocalDate;
 import java.util.Scanner;
+import java.time.Instant;
 
 public class MortgageCalculator{
     final static byte monthsInAYear = 12;
     final static byte percent = 100;
-
+    // Get the current date
+    static LocalDate currentDate = LocalDate.now();
     public static void main (String args[])
     {
 
-        float principal= (float)readNumber("Principal: $ ", 1000, 1000000);
-        float annualInterestRate = (float)readNumber("Annual Interest Rate (%):  ", 0.1, 30);
-        float periodYears = (float)readNumber("Years: ", 1, 30);
-        System.out.println();
-        System.out.println("MORTGAGE");
-        System.out.println("________");
-        System.out.println("Your mortgage is: $ " + calculateMortgage(principal, annualInterestRate, periodYears)); // today's date integration
-        System.out.println();
-        System.out.println("PAYMENT SCHEDULE");
-        System.out.println("________________");
+        double principal= readNumber("Principal: $ ", 1000, 1000000);
+        double annualInterestRate = readNumber("Annual Interest Rate (%):  ", 0.1, 30);
+        double periodYears =readNumber("Years: ", 1, 30);
+        printMortgage(principal, annualInterestRate, periodYears); //created using refactor >>> extract method to reduce LOC in main method
+        printPaymentSchedule(periodYears, principal, annualInterestRate); //created using refactor >>> extract method to reduce LOC in main method
+    }
+
+    private static void printPaymentSchedule(double periodYears, double principal, double annualInterestRate) {
+        System.out.println("\n PAYMENT SCHEDULE \n -------------------\n");
         for (short month = 1; month <= periodYears * monthsInAYear; month++){
             double balance = calculateBalance(principal, annualInterestRate, periodYears, month); // why month???? >>> this will eventually attribute to the fourth parameter in calculateBalance()
-            //increment date by 30 days
-            System.out.println("$ " + balance);
+            // Add one month to the current date
+            currentDate=currentDate.plusMonths(1);
+            System.out.println("$ " + balance + "\tthe next installment is at \t" +currentDate);
         }
     }
+
+    private static void printMortgage(double principal, double annualInterestRate, double periodYears) {
+        System.out.println("\nMORTGAGE\n-------------");
+        System.out.println("Your mortgage contract of : $ " + calculateMortgage(principal, annualInterestRate, periodYears)+"\ton "+currentDate); // today's date integration
+    }
+
     public static double readNumber(String prompt, double min, double max){
         Scanner scanner = new Scanner(System.in);
         double value;
@@ -35,16 +44,15 @@ public class MortgageCalculator{
                 break;
             System.out.println("Enter a number between " + min + " and " + max);}
         return value;
-
     }
 
     //mortgage calculator
     public static double calculateMortgage(
-            float principal,
-            float annualInterestRate,
-            float periodYears) //ask M why redundant?
+            double principal,
+            double annualInterestRate,
+            double periodYears) //ask M why redundant?
     {
-        float numberOfPayments = periodYears * monthsInAYear; // numberOfPayments
+        double numberOfPayments = periodYears * monthsInAYear; // numberOfPayments
         double monthlyInterestRate = annualInterestRate / (monthsInAYear * percent); // monthlyInterestRate
 
         double mortgage = principal * (monthlyInterestRate * Math.pow(1+ monthlyInterestRate, numberOfPayments) / (Math.pow(1+ monthlyInterestRate , numberOfPayments) - 1));
@@ -53,12 +61,12 @@ public class MortgageCalculator{
     }
 
     public static double calculateBalance(
-            float principal,
-            float annualInterestRate,
-            float periodYears,
+            double principal,
+            double annualInterestRate,
+            double periodYears,
             short numberOfPaymentsMade){
 
-        float numberOfPayments = periodYears * monthsInAYear; // numberOfPayments
+        short numberOfPayments = (short) (periodYears * monthsInAYear); // numberOfPayments
         double monthlyInterestRate = annualInterestRate / (monthsInAYear * percent); // monthlyInterestRate
 
         double balance = principal
--- clean.java
+++
@@ -1,9 +0,0 @@
-public class clean {
-    public static void clean(String[] args) {
-        greetUser ("Mosh");
-    }
-
-    public static void greetUser(String name){
-        System.out.println("Hello" + name);
-    }
-}
